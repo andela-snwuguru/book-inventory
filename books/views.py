44e5fb07cf6a87463efcce9b05c6a2d17f068d47
Nwuguru Sunday <sunday.nwuguru@andela.com>
2016-08-25 12:19:16 +0100

    Add search by name and category

--- books/views.py
+++ books/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, ListView
 
-from .models import Book
+from .models import Book, Category
 
 
 class BookListView(ListView):
     model = Book
     template_name = 'book_list.html'
+
+    def get_context_data(self, **kwargs):
+        q = self.request.GET.get('q', '')
+        category_id = self.request.GET.get('cat', '')
+        print category_id
+        context = super(BookListView, self).get_context_data(**kwargs)
+        print q, category_id
+        context['q'] = q
+        context['category_id'] = category_id
+        context['categories'] = Category.objects.all()
+        return context
+
+    def get_queryset(self):
+        q = self.request.GET.get('q', '')
+        category_id = self.request.GET.get('cat')
+        if category_id:
+            category = Category.objects.get(pk=category_id)
+            return Book.objects.filter(category=category, name__contains=q)
+        return Book.objects.filter(name__contains=q)
 
 
 class BookCreateView(CreateView):
@@ -14,3 +33,4 @@ class BookCreateView(CreateView):
     fields = ['name', 'category']
     template_name = 'book_form.html'
     success_url = '/books/'
+
